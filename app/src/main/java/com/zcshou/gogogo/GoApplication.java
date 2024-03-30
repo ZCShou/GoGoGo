9aff736220029724329cbcb02b5fbf50eb45a5e4
ZCShou <72115@163.com>
2024-03-30 16:09:12 +0800

    chore(main): clean warnings

--- app/src/main/java/com/zcshou/gogogo/GoApplication.java
+++ app/src/main/java/com/zcshou/gogogo/GoApplication.java
@@ -28,6 +28,8 @@ public class GoApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        initXlog();
+
         // 百度地图 7.5 开始，要求必须同意隐私政策，默认为false
         SDKInitializer.setAgreePrivacy(this, true);
         // 百度定位 7.5 开始，要求必须同意隐私政策，默认为false(官方说可以统一为以上接口，但实际测试并不行，定位还是需要单独设置)
@@ -38,10 +40,8 @@ public class GoApplication extends Application {
             SDKInitializer.initialize(this);
             SDKInitializer.setCoordType(CoordType.BD09LL);
         } catch (BaiduMapSDKException e) {
-            e.printStackTrace();
+            XLog.e("ERROR init baidu sdk");
         }
-
-        initXlog();
     }
 
     /**
@@ -49,12 +49,13 @@ public class GoApplication extends Application {
      */
     private void initXlog() {
         File logPath = getExternalFilesDir("Logs");
-        LogConfiguration config = new LogConfiguration.Builder()
-                .logLevel(LogLevel.ALL)
-                .tag(APP_NAME)                                         // 指定 TAG，默认为 "X-LOG"
-                .enableThreadInfo()                                    // 允许打印线程信息，默认禁止
-                .enableStackTrace(2)                                   // 允许打印深度为 2 的调用栈信息，默认禁止
-                .enableBorder()                                        // 允许打印日志边框，默认禁止
+        if (logPath != null) {
+            LogConfiguration config = new LogConfiguration.Builder()
+                    .logLevel(LogLevel.ALL)
+                    .tag(APP_NAME)                                         // 指定 TAG，默认为 "X-LOG"
+                    .enableThreadInfo()                                    // 允许打印线程信息，默认禁止
+                    .enableStackTrace(2)                                   // 允许打印深度为 2 的调用栈信息，默认禁止
+                    .enableBorder()                                        // 允许打印日志边框，默认禁止
 //                .jsonFormatter(new MyJsonFormatter())                  // 指定 JSON 格式化器，默认为 DefaultJsonFormatter
 //                .xmlFormatter(new MyXmlFormatter())                    // 指定 XML 格式化器，默认为 DefaultXmlFormatter
 //                .throwableFormatter(new MyThrowableFormatter())        // 指定可抛出异常格式化器，默认为 DefaultThrowableFormatter
@@ -66,16 +67,16 @@ public class GoApplication extends Application {
 //                .addInterceptor(new BlacklistTagsFilterInterceptor(    // 添加黑名单 TAG 过滤器
 //                        "blacklist1", "blacklist2", "blacklist3"))
 //                .addInterceptor(new MyInterceptor())                   // 添加一个日志拦截器
-                .build();
+                    .build();
 
-//        Printer androidPrinter = new AndroidPrinter(true);  // 通过 android.util.Log 打印日志的打印器
-        Printer consolePrinter = new ConsolePrinter();                  // 通过 System.out 打印日志到控制台的打印器
-        Printer filePrinter = new FilePrinter                           // 打印日志到文件的打印器
-                .Builder(logPath.getPath())                             // 指定保存日志文件的路径
-                .fileNameGenerator(new ChangelessFileNameGenerator(LOG_FILE_NAME))         // 指定日志文件名生成器，默认为 ChangelessFileNameGenerator("log")
-                .backupStrategy(new NeverBackupStrategy())              // 指定日志文件备份策略，默认为 FileSizeBackupStrategy(1024 * 1024)
-                .cleanStrategy(new FileLastModifiedCleanStrategy(MAX_TIME))     // 指定日志文件清除策略，默认为 NeverCleanStrategy()
-                .build();
-        XLog.init(config, consolePrinter, filePrinter);
+            Printer consolePrinter = new ConsolePrinter();                  // 通过 System.out 打印日志到控制台的打印器
+            Printer filePrinter = new FilePrinter                           // 打印日志到文件的打印器
+                    .Builder(logPath.getPath())                             // 指定保存日志文件的路径
+                    .fileNameGenerator(new ChangelessFileNameGenerator(LOG_FILE_NAME))         // 指定日志文件名生成器，默认为 ChangelessFileNameGenerator("log")
+                    .backupStrategy(new NeverBackupStrategy())              // 指定日志文件备份策略，默认为 FileSizeBackupStrategy(1024 * 1024)
+                    .cleanStrategy(new FileLastModifiedCleanStrategy(MAX_TIME))     // 指定日志文件清除策略，默认为 NeverCleanStrategy()
+                    .build();
+            XLog.init(config, consolePrinter, filePrinter);
+        }
     }
 }
